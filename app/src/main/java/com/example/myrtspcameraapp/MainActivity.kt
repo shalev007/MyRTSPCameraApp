96855633d6ee0e141f9d43a6f3a2ff7f9a2003f3
Shalev Avhar <shalev@getport.io>
2024-07-01 17:01:48 +0300

    feat: add switches

--- app/src/main/java/com/example/myrtspcameraapp/MainActivity.kt
+++ app/src/main/java/com/example/myrtspcameraapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myrtspcameraapp;
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
@@ -22,19 +26,33 @@ class MainActivity : AppCompatActivity() {
         setupPlayer()
     }
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     @OptIn(UnstableApi::class)
     private fun setupPlayer() {
         player = ExoPlayer.Builder(this).build()
         val playerView = findViewById<PlayerView>(R.id.player_view)
+        val uriInput = findViewById<EditText>(R.id.uri_input)
+        val transportSwitch = findViewById<Switch>(R.id.transport_switch)
+        val playButton = findViewById<Button>(R.id.play_button)
+//        val rtspUri = "rtsp://51.17.227.45:5555/av0_0"
         playerView.player = player
-//        val rtspUri = "rtsp://fake.kerberos.io/stream"
-        val rtspUri = "rtsp://51.17.227.45:5555/av0_0"
-        val mediaItem = MediaItem.fromUri(rtspUri)
-//        player?.setMediaItem(mediaItem)
-        val mediaSource: MediaSource = RtspMediaSource.Factory().setForceUseRtpTcp(true).createMediaSource(mediaItem)
-        player?.setMediaSource(mediaSource)
-        player?.prepare()
-        player?.play()
+        playButton.setOnClickListener {
+            val uri = uriInput.text.toString()
+            val useTcp = transportSwitch.isChecked
+
+            if (uri.isNotEmpty()) {
+                val mediaItem = MediaItem.fromUri(uri)
+                val rtspDataSourceFactory = RtspMediaSource.Factory()
+                    .setForceUseRtpTcp(useTcp)
+
+                val mediaSource = rtspDataSourceFactory.createMediaSource(mediaItem)
+
+                player?.setMediaSource(mediaSource)
+                player?.prepare()
+                player?.play()
+
+            }
+        }
     }
 
     override fun onDestroy() {
